87f754ed3df01fd41a9bfad31bb2a66d48d5a57f
Mateo2131 <goz.matu@gmail.com>
2023-02-03 22:10:30 -0300

    fix: navbar link on small screens

--- src/layout/Navbar/Link.tsx
+++ src/layout/Navbar/Link.tsx
@@ -10,7 +10,7 @@ function NavbarLink({ text, href }: Props) {
     <li className='h-full'>
       <Link
         href={href}
-        className='flex items-center h-full mb-1 border-b-2 border-transparent text-brand-subtext hover:border-b-brand-mauve'
+        className='flex items-center h-full mb-1 border-b-2 border-transparent text-brand-subtext sm:hover:border-b-brand-mauve'
       >
         {text}
       </Link>
