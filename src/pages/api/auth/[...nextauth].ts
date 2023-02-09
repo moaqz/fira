23753b291e67484b2eb7bd42b3707153245d079a
Mateo2131 <goz.matu@gmail.com>
2023-02-09 18:38:52 -0300

    feat: add nextauth secret

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -12,7 +12,8 @@ export const authOptions: NextAuthOptions = {
       clientId: process.env.GITHUB_ID as string,
       clientSecret: process.env.GITHUB_CLIENT_SECRET as string
     })
-  ]
+  ],
+  secret: process.env.SECRET
 }
 
 export default NextAuth(authOptions)
