779cadf03fa94b48cd8a9eebe35f21603e1696d4
techwithmat <goz.matu@gmail.com>
2023-05-09 20:50:35 -0300

    refactor: remove swr and nextSeo

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,18 +1,16 @@
 "use client";
 
-import NextSEOProviders from "./nextSEO";
+
 import SessionProvider from "./nextauth";
-import SWRProvider from "./swr";
 import ToasterProvider from "./toaster";
 import ReactQueryProvider from "./react-query";
 
 function Providers({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <NextSEOProviders />
       <SessionProvider>
         <ReactQueryProvider>
-          <SWRProvider>{children}</SWRProvider>
+          {children}
         </ReactQueryProvider>
       </SessionProvider>
       <ToasterProvider />
