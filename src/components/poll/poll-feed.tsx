80777bdb4841669078ed85f07b95202aa0999404
techwithmat <goz.matu@gmail.com>
2023-05-09 21:16:51 -0300

    fix: empty state not showing correctly

--- src/components/poll/poll-feed.tsx
+++ src/components/poll/poll-feed.tsx
@@ -20,31 +20,39 @@ function PollFeed() {
       staleTime: 1000 * 3,
     });
 
-  const polls = data?.pages.flatMap((poll) => poll.polls) ?? [];
+  const polls = data?.pages.flatMap((poll) => poll.polls);
+
+  if (isLoading) {
+    return <LineLoader text="Fetching data..." />;
+  }
 
   return (
-    <div className="space-y-4">
-      {!isLoading && !isFetching && isError && (
+    <div className="mb-20 space-y-4">
+      {!isFetching && isError && (
         <div className="mt-4 flex items-center justify-center space-x-3 rounded border border-red-300 p-4 text-red-300">
           <ExclamationTriangleIcon />
           <span className="text-lg">Error fetching data</span>
         </div>
       )}
 
-      {isLoading && <LineLoader text="Fetching data..." />}
-
-      {polls.map((poll) => {
-        return (
-          <PollCard
-            key={poll.id + poll.title}
-            title={poll.title}
-            id={poll.id}
-            totalCount={poll.totalCount}
-            createdAt={poll.createdAt}
-            endsAt={poll.endsAt}
-          />
-        );
-      })}
+      {polls && polls.length > 0 ? (
+        polls.map((poll) => {
+          return (
+            <PollCard
+              key={poll.id + poll.title}
+              title={poll.title}
+              id={poll.id}
+              totalCount={poll.totalCount}
+              createdAt={poll.createdAt}
+              endsAt={poll.endsAt}
+            />
+          );
+        })
+      ) : (
+        <p className="mt-4 text-center text-lg font-semibold text-gray-400">
+          You have not created any polls yet.
+        </p>
+      )}
 
       {hasNextPage && !isFetching && !isError && (
         <Button className="w-full" onClick={() => fetchNextPage()}>
@@ -52,9 +60,9 @@ function PollFeed() {
         </Button>
       )}
 
-      {!isFetching && !isError && !hasNextPage && (
-        <p className="text-center font-semibold text-gray-400">
-          You{"'"}ve reached the end of the list
+      {!isFetching && !isError && !hasNextPage && polls && polls.length > 0 && (
+        <p className="mt-4 text-center text-lg font-semibold text-gray-400">
+          You{"'"}ve reached the end of the list.
         </p>
       )}
     </div>
