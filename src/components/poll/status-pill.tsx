77a4d2bd71a564b32e00c949b6c8572bcbd6699e
techwithmat <goz.matu@gmail.com>
2023-05-10 17:25:20 -0300

    feat: modify status pill colors

--- src/components/poll/status-pill.tsx
+++ src/components/poll/status-pill.tsx
@@ -1,8 +1,8 @@
 import clsx from "clsx";
 
 const STATUS = {
-  live: "bg-green-500 text-white",
-  closed: "bg-red-500 text-white",
+  live: "bg-green-800 text-white",
+  closed: "bg-red-700 text-white",
 };
 
 export enum PollStatus {
@@ -11,8 +11,10 @@ export enum PollStatus {
 }
 
 function StatusPill({ status }: { status: "closed" | "live" }) {
+  const statusText = status === PollStatus.CLOSED ? "Closed" : "Live";
+
   return (
-    <div
+    <span
       className={clsx(
         "flex w-24 flex-grow-0 items-center justify-center gap-2 rounded-full px-4 py-2 text-sm font-medium",
         STATUS[status],
@@ -21,8 +23,8 @@ function StatusPill({ status }: { status: "closed" | "live" }) {
       <svg className="h-2 w-2" fill="currentColor" viewBox="0 0 8 8">
         <circle cx="4" cy="4" r="3"></circle>
       </svg>
-      {status === PollStatus.CLOSED ? "Closed" : "Live"}
-    </div>
+      {statusText}
+    </span>
   );
 }
 
