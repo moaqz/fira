e25c27ade568a59a1119bf0760b1e9867114123d
techwithmat <goz.matu@gmail.com>
2023-05-12 12:37:27 -0300

    feat(footer): center the text

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -1,19 +1,15 @@
 function Footer() {
   return (
     <footer className="fixed bottom-0 z-50 w-full px-5 text-brand-subtext">
-      <div className="flex flex-col items-center justify-between py-4 sm:flex-row">
-        <p>Made using TailwindCSS & Next.js</p>
-        <div className="flex items-center space-x-1">
-          <p>Crafted by</p>
-          <a
-            href="https://github.com/techwithmat"
-            target="_blank"
-            rel="noreferrer"
-            className="flex items-center gap-2 transition-colors duration-100 hover:text-white"
-          >
-            techwithmat
-          </a>
-        </div>
+      <div className="flex items-center justify-center py-4">
+        <a
+          href="https://github.com/techwithmat"
+          target="_blank"
+          rel="noreferrer"
+          className="flex items-center gap-2 transition-colors duration-100 hover:text-white"
+        >
+          Crafted by techwithmat
+        </a>
       </div>
     </footer>
   );
